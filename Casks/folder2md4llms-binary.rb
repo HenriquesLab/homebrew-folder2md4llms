99cdc7c97917db321a8f8d1454fc91dfcd081833
Ricardo Henriques <paxcalpt@gmail.com>
2025-07-25 12:00:08 +0100

    chore: update binary cask to version 0.5.0

--- Casks/folder2md4llms-binary.rb
+++ Casks/folder2md4llms-binary.rb
@@ -1,7 +1,7 @@
 cask "folder2md4llms-binary" do
-  version "0.4.37"
-  sha256 arm:   "1c4da79e58124eff1ba402fd27cc6166d4070a102b9f0a3c61762faccd4cce70",
-         intel: "b3454ad33998689ff93732d80d395d94a7bc022706f2a3e519082bb7c8dfaee9"
+  version "0.5.0"
+  sha256 arm:   "ed2634ebd5abd868b983c391f7c7e85136a138cbab82106a48b24cef8baa2597",
+         intel: "f72196c13e9a74fe2cda9cc591ed7274600c23235ba2606aef76977700ec57bc"
 
   url "https://github.com/HenriquesLab/folder2md4llms/releases/download/v#{version}/folder2md-macos-#{Hardware::CPU.intel? ? "x64" : "arm64"}"
   name "folder2md4llms"
